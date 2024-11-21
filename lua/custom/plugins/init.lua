8a6a61525853efd14d7fd98d2c17f5e3b5f77982
harlin.windham <1855418+harlinw@users.noreply.github.com>
2024-11-21 14:34:18 -0800

    lazy git

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+--[[
+winget install -e 
+  --id=JesseDuffield.lazygit
+  --id=BurntSushi.ripgrep.MSVC
+--]]
 return {}
